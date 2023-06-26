3b4d3c68da92ed6bcc29cad30484259e46920b77
anish0123 <anishmhn@hotmail.com>
2023-06-26 22:11:33 +0300

    css styling project images and code formatting in index.css

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,22 +4,19 @@ const Project = ({ project }) => {
   return (
     <ProjectDiv>
       <ProjectHolder>
-        <div className="singleProject">
-          <img src={`${project.image}`}  alt={`${project.title} logo`} className="projectImage" />
+        <SingleProject>
+          <ProjectImg src={`${project.image}`} alt={`${project.title} logo`} />
           <div className="projectInfo">
             <h4 className="projectTitle"> {project.title}</h4>
             <h6 className="projectPara">{project.description}</h6>
             <h6 className="projectLink">
               Github Link:{" "}
-              <a
-                className="links"
-                href={`${project.projectLink}`}
-              >
+              <a className="links" href={`${project.projectLink}`}>
                 {project.title}
               </a>
             </h6>
           </div>
-        </div>
+        </SingleProject>
       </ProjectHolder>
     </ProjectDiv>
   );
@@ -31,7 +28,6 @@ const ProjectDiv = styled.div`
   flex: 1;
   flex-direction: column;
   padding-top: 5rem;
-  
 
   @media only screen and (max-width: 1080px) {
     width: 100%;
@@ -45,7 +41,7 @@ const ProjectDiv = styled.div`
 `;
 
 const ProjectHolder = styled.div`
-display: flex;
+  display: flex;
   flex: 3;
   flex-direction: column;
   justify-content: left;
@@ -64,7 +60,52 @@ display: flex;
     align-items: center;
     margin-left: 10%;
   }
+`;
 
+const SingleProject = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  margin-top: 5rem;
+  margin-right: 10%;
+  margin-left: 10%;
+  margin-bottom: 0;
+  padding: 0;
+
+  @media only screen and (max-width: 1080px) {
+    justify-content: center;
+  }
+
+  @media only screen and (max-width: 900px) {
+    align-items: center;
+  }
+
+  @media only screen and (max-width: 500px) {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const ProjectImg = styled.img`
+  width: 7em;
+  background-color: white;
+  height: 10rem;
+  margin-right: 2%;
+  margin-top: 10px;
+
+  @media only screen and (max-width: 1080px) {
+    height: 5rem;
+    margin-top: 2rem;
+  }
+
+  @media only screen and (max-width: 900px) {
+    height: 4rem;
+    margin-right: 2%;
+  }
+
+  @media only screen and (max-width: 500px) {
+    width: 5em;
+  }
 `;
 
 export default Project;
