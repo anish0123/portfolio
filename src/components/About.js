a2803317a041cecb7ca38610c956b61222fa30e2
anish0123 <anishmhn@hotmail.com>
2024-10-28 18:15:59 +0200

    updated the portfolio with latest info

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,24 @@
 import { styled } from "styled-components";
 import Lottie from "lottie-react";
-import helloWorld from "../Lottie/helloWorld.json"
-import StyledHeading from "./StyledHeading"
+import helloWorld from "../Lottie/helloWorld.json";
+import StyledHeading from "./StyledHeading";
 
 const About = () => {
   return (
     <AboutMe id="about">
       <StyledHeading>About Me</StyledHeading>
       <StyledParaDiv>
-      <LottieDiv>
-      <Lottie animationData={helloWorld} />
-      </LottieDiv>
-      <ProfilePara>
-        I am third-year IT student in Metropolia University of Applied
-        Sciences, currently learning and gaining knowledge in various
-        programming languages and technologies. I am proficient in
-        problem-solving and analytical thinking, and I am constantly working to
-        improve my skills and stay up-to-date with the latest advancements in
-        the field. I am currently seeking internships or entry-level positions
-        to gain hands-on experience and contribute to the development of
-        innovative software.
-      </ProfilePara>
-      
+        <LottieDiv>
+          <Lottie animationData={helloWorld} />
+        </LottieDiv>
+        <ProfilePara>
+          I am fourth-year IT student in Metropolia University of Applied
+          Sciences, currently learning and gaining knowledge in various
+          programming languages and technologies. Since August 2023, I have been
+          developing my skills as junior software developer at TOYOTA GAZOO
+          Racing World Rally Team Oy, where I apply my skills to real-world
+          projects in a dynamic environment.
+        </ProfilePara>
       </StyledParaDiv>
 
       <StyledHeading>My Tech Stack</StyledHeading>
@@ -50,140 +47,133 @@ const AboutMe = styled.div`
 
   @media only screen and (max-width: 1440px) {
     padding-top: 3%;
-   
   }
 
   @media only screen and (max-width: 768px) {
     height: 110vh;
-   
   }
 
   @media only screen and (max-width: 390px) {
     height: 90vh;
-   
   }
 `;
 
-
 const ProfilePara = styled.p`
-width: 40%;
-font-size: 1.5rem;
-font-weight: bold;
-margin-left : 10%;
-
-@media only screen and (max-width: 1440px) {
-  width: 80%;
+  width: 40%;
   font-size: 1.5rem;
-  margin-right: 5%;
-}
+  font-weight: bold;
+  margin-left: 10%;
 
-@media only screen and (max-width: 1080px) {
-  width: 80%;
-  font-size: 1.8rem;
-  margin-left: 6%;
-}
+  @media only screen and (max-width: 1440px) {
+    width: 80%;
+    font-size: 1.5rem;
+    margin-right: 5%;
+  }
 
-@media only screen and (max-width: 900px) {
-  width: 90%;
-  font-size: 1.5rem;
-  margin-left: 6%;
-}
+  @media only screen and (max-width: 1080px) {
+    width: 80%;
+    font-size: 1.8rem;
+    margin-left: 6%;
+  }
 
-@media only screen and (max-width: 750px) {
-  font-size: 1.2rem;
-}
+  @media only screen and (max-width: 900px) {
+    width: 90%;
+    font-size: 1.5rem;
+    margin-left: 6%;
+  }
 
-@media only screen and (max-width: 500px) {
-  font-size: 1rem;
-  margin-left: 15%;
-}
+  @media only screen and (max-width: 750px) {
+    font-size: 1.2rem;
+  }
 
-@media only screen and (max-width: 440px) {
-  font-size: 0.9rem;
-  margin-left: 12%;
-}
+  @media only screen and (max-width: 500px) {
+    font-size: 1rem;
+    margin-left: 15%;
+  }
 
-@media only screen and (max-width: 380px) {
-  font-size: 1rem;
-}
+  @media only screen and (max-width: 440px) {
+    font-size: 0.9rem;
+    margin-left: 12%;
+  }
+
+  @media only screen and (max-width: 380px) {
+    font-size: 1rem;
+  }
 `;
 
 const StyledParaDiv = styled.div`
-display: flex; 
- margin: 0; 
- padding: 0;
+  display: flex;
+  margin: 0;
+  padding: 0;
 
- @media only screen and (max-width: 500px) {
-  flex-direction : column;
-  align-items: center;
-  justify-content: center;
-}
-
+  @media only screen and (max-width: 500px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 const LottieDiv = styled.div`
-width: 25%;
-margin-left: 15%;
+  width: 25%;
+  margin-left: 15%;
 
-@media only screen and (max-width: 1440px) {
-  width: 50%;
- padding-top: 5%;
-}
+  @media only screen and (max-width: 1440px) {
+    width: 50%;
+    padding-top: 5%;
+  }
 
-@media only screen and (max-width: 1080px) {
-  width: 70%;
- padding-top: 15%;
- 
-}
+  @media only screen and (max-width: 1080px) {
+    width: 70%;
+    padding-top: 15%;
+  }
 
-@media only screen and (max-width: 500px) {
-  width: 80%;
-  padding-top: 20%;
-  padding-right: 20%;
-}
+  @media only screen and (max-width: 500px) {
+    width: 80%;
+    padding-top: 20%;
+    padding-right: 20%;
+  }
 
-@media only screen and (max-width: 440px) {
-  width: 80%;
-  padding-top: 30%;
-}
+  @media only screen and (max-width: 440px) {
+    width: 80%;
+    padding-top: 30%;
+  }
 `;
 
 const Icons = styled.img`
-    width: 80rem;
-    padding: 0;
+  width: 80rem;
+  padding: 0;
 
-    @media only screen and (max-width: 1440px) {
-      width: 75rem;
-    }
+  @media only screen and (max-width: 1440px) {
+    width: 75rem;
+  }
 
-    @media only screen and (max-width: 1080px) {
-      width: 58rem;
-    }
+  @media only screen and (max-width: 1080px) {
+    width: 58rem;
+  }
 
-    @media only screen and (max-width: 900px) {
-      width: 50rem;
-    }
+  @media only screen and (max-width: 900px) {
+    width: 50rem;
+  }
 
-    @media only screen and (max-width: 750px) {
-      width: 42rem;
-    }
+  @media only screen and (max-width: 750px) {
+    width: 42rem;
+  }
 
-    @media only screen and (max-width: 500px) {
-      width: 28rem;
-    }
+  @media only screen and (max-width: 500px) {
+    width: 28rem;
+  }
 
-    @media only screen and (max-width: 440px) {
-      width: 24rem;
-    }
+  @media only screen and (max-width: 440px) {
+    width: 24rem;
+  }
 
-    @media only screen and (max-width: 380px) {
-      width: 21rem;
-    }
+  @media only screen and (max-width: 380px) {
+    width: 21rem;
+  }
 
-    @media only screen and (max-width: 300px) {
-      width: 18rem;
-    }
+  @media only screen and (max-width: 300px) {
+    width: 18rem;
+  }
 `;
-
 
 export default About;
